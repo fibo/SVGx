25ee07b13241c36b4f61a86fd1b5c38393e015b6
fibo <casati_gianluca@yahoo.it>
2016-04-27 17:33:42 -0400

    added dummy Svg component

---
+++ src/components/Svg.js
@@ -0,0 +1,6 @@
+export default (React) => (props) => {
+  <svg xmlns='http://www.w3.org/svg/2000'
+     width={props.width}
+     height={props.height}>
+  <svg>
+}
---
+++ test/components/Svg.js
@@ -0,0 +1,3 @@
+var test = require('tape')
+var createSvg = require('components/Svg')
+
