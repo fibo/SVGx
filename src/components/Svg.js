6ca52df23c7fd3485f6f8247e07ae06467b55594
fibo <casati_gianluca@yahoo.it>
2016-04-28 17:28:18 -0400

    added test

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -1,6 +1,19 @@
-export default (React) => (props) => {
-  <svg xmlns='http://www.w3.org/svg/2000'
-     width={props.width}
-     height={props.height}>
-  <svg>
+import React, { PropTypes } from 'react'
+
+const Svg = ({width, height}) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/svg/2000'
+      width={width}
+      height={height}
+    >
+    </svg>
+  )
 }
+
+Svg.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
+}
+
+export default Svg
--- test/components/Svg.js
+++ test/components/Svg.js
@@ -1,3 +1,21 @@
-var test = require('tape')
-var createSvg = require('components/Svg')
+import dom from 'cheerio'
+import React from 'react'
+import reactDom from 'react-dom/server'
+import Svg from 'components/Svg'
+import test from 'tape'
 
+const render = reactDom.renderToStaticMarkup
+
+test('Svg', (t) => {
+  const height = 100
+  const width = 100
+
+  const el = <Svg width={width} height={height}/>
+
+  const $ = dom.load(render(el), { xmlMode: true })
+
+  t.equal($('svg').attr('width'), width.toString(), 'width prop')
+  t.equal($('svg').attr('height'), height.toString(), 'height prop')
+
+  t.end()
+})
