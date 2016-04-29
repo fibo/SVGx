43ecf7f1e797ace9def86d4588d9a0ad61aa4d76
fibo <casati_gianluca@yahoo.it>
2016-04-29 18:03:44 -0400

    fixed svg

--- example.js
+++ example.js
@@ -6,7 +6,10 @@ import path from 'path'
 import { render } from 'svgx'
 
 const svgOutput = render(
-  <svg width={100} height={100}>
+  <svg xmlns='http://www.w3.org/svg/2000'
+    width={100}
+    height={100}
+  >
     <rect x={10} y={10} width={50} height={50} />
   </svg>
 )
