f076e48c6589ba91e682495a9d6260240002278a
fibo <casati_gianluca@yahoo.it>
2016-04-30 08:42:37 -0400

    added xmlns

--- src/svgx.js
+++ src/svgx.js
@@ -1,5 +1,14 @@
+import dom from 'cheerio'
 import reactDom from 'react-dom/server'
 
-const render = reactDom.renderToStaticMarkup
+const render = (jsx) => {
+  let result = reactDom.renderToStaticMarkup(jsx)
+
+  let $ = dom.load(result, { xmlMode: true })
+
+  $('svg').attr('xlmns', 'http://www.w3.org/svg/2000')
+
+  return $.html()
+}
 
 export { render }
