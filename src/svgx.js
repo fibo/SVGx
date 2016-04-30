4ebfdff947cf7106a0055d52735b3f1a694ca666
fibo <casati_gianluca@yahoo.it>
2016-04-30 08:50:45 -0400

    added xlink

--- src/svgx.js
+++ src/svgx.js
@@ -7,6 +7,7 @@ const render = (jsx) => {
   let $ = dom.load(result, { xmlMode: true })
 
   $('svg').attr('xmlns', 'http://www.w3.org/svg/2000')
+  $('svg').attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
 
   return $.html()
 }
