77542f011afb89d8f482d96f23d60307a9a5203c
fibo <casati_gianluca@yahoo.it>
2019-04-18 10:32:51 +0200

    added newlines to make svg valid on GitHub

--- svgx.js
+++ svgx.js
@@ -5,14 +5,13 @@ function svgx (render) {
       opts = {
         doctype: [
           '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
-          '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
-        ].join(' ')
+          '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+          ''
+        ].join('\n')
       }
     }
 
-    const svg = render(jsx)
-
-    return opts.doctype + ' ' + svg
+    return opts.doctype + render(jsx)
   }
 }
 
--- test.js
+++ test.js
@@ -15,7 +15,14 @@ test('svgx.render(jsx)', (t) => {
     width={width} height={height}
   />
 
-  t.equal(render(el), `<?xml version="1.0" encoding="UTF-8" standalone="no"?> <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100"></svg>`)
+  t.equal(
+    render(el),
+    [
+      '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
+      '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+      '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100"></svg>'
+    ].join('\n')
+  )
 
   t.end()
 })
